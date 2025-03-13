89553a8dfc9919096e91badae627fe89b389d3a3
Sikander <139198327+Yash-coder53@users.noreply.github.com>
2025-03-13 22:24:16 +0530

    Create holi-banner.sh

---
+++ holi-banner.sh
@@ -0,0 +1,13 @@
+bash
+#!/bin/bash
+
+Holi banner
+echo " "
+echo "   __  __       _   _   _ "
+echo "  |  \/  | ___ | |_| |_| |"
+echo "  | |\/| |/ _ \| __| __| |"
+echo "  | |  | | (_) | |_| |_|_|"
+echo "  |_|  |_|\___/ \__|\__|(_)"
+echo " "
+echo "  Holi Hai! "
+echo " "
